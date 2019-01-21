6301767221b10f65da22d6c523a2d8d30c2ac512
Ravi Teja D <ravi1917d@gmail.com>
2019-01-21 17:26:38 +0530

    RED STATE - Checking for button rendering on App page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("AppMain", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("should render a signIn button in App", () => {
+    let wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<SignInButton />)).toEqual(true);
+  });
 });
